3260193ae687204b1c5b4af14c8022b6a64425db
Abdurrab Khan <67190463+AbdurrabKhan198@users.noreply.github.com>
2025-08-30 15:00:07 +0530

    Update settings.py

--- doctor/settings.py
+++ doctor/settings.py
@@ -119,8 +119,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+# Where collectstatic will put all files for production (AWS / server)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
